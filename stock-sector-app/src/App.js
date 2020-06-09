5fe1f4182aa2f3af5683e3f22752fcda44d5a9fd
B CASTILLO <bryancastillo44@outlook.com>
2020-06-09 00:51:44 -0400

    Minimal App functionality

--- stock-sector-app/src/App.js
+++ stock-sector-app/src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import { Route, Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
-import SectorList from "./SectorList";
-import Sector from "./Sector";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import Sector from "./Sector";
+import "./App.css";
 
 class App extends Component {
 
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
 
     this.state = {
       title: [], 
@@ -37,22 +36,111 @@ render(){
 
   // console.log(this.state)
 
+  let {sectors} = this.state;
+
   return (
-    
     
     <div className="container">
 
-       <Route path="/"
-          exact 
-          render={() => {
-              return <SectorList sectors={this.state.sectors}>
-              </SectorList> 
-            }}
-            />
+      <h3>Energy Sector</h3>
       
+        {sectors.map(sector => (
+          <p key={sector.id}>
+        {sector.title} : {sector.sectors[0].Energy}%
+          </p>
+        ))}
+      <br></br>
 
-     
+    <h3>Financial Sector</h3>
       
+      {sectors.map(sector => (
+        <p key={sector.id}>
+      {sector.title} : {sector.sectors[0].Financials}%
+        </p>
+      ))}
+  <br></br>
+        
+    <h3>Industrials Sector</h3>
+      
+      {sectors.map(sector => (
+        <p key={sector.id}>
+      {sector.title} : {sector.sectors[0].Industrials}%
+        </p>
+      ))}
+
+<h3>Real Estate</h3>
+      
+      {sectors.map(sector => (
+        <p key={sector.id}>
+      {sector.title} : {sector.sectors[0].RealEstate}%
+        </p>
+      ))}
+  <br></br>
+
+  <h3>Information Technology</h3>
+      
+      {sectors.map(sector => (
+        <p key={sector.id}>
+      {sector.title} : {sector.sectors[0].InformationTechnology}%
+        </p>
+      ))}
+  <br></br>
+
+  <h3>Materials</h3>
+      
+      {sectors.map(sector => (
+        <p key={sector.id}>
+      {sector.title} : {sector.sectors[0].Materials}%
+        </p>
+      ))}
+  <br></br>
+
+  <h3>Consumer Discretionary</h3>
+      
+      {sectors.map(sector => (
+        <p key={sector.id}>
+      {sector.title} : {sector.sectors[0].ConsumerDiscretionary}%
+        </p>
+      ))}
+  <br></br>
+
+  <h3>Communication Services</h3>
+      
+      {sectors.map(sector => (
+        <p key={sector.id}>
+      {sector.title} : {sector.sectors[0].CommunicationServices}%
+        </p>
+      ))}
+  <br></br>
+
+  <h3>Health Care</h3>
+      
+      {sectors.map(sector => (
+        <p key={sector.id}>
+      {sector.title} : {sector.sectors[0].HealthCare}%
+        </p>
+      ))}
+  <br></br>
+
+  <h3>Consumer Staples</h3>
+      
+      {sectors.map(sector => (
+        <p key={sector.id}>
+      {sector.title} : {sector.sectors[0].ConsumerStaples}%
+        </p>
+      ))}
+  <br></br>
+
+  <h3>Utilities</h3>
+      
+      {sectors.map(sector => (
+        <p key={sector.id}>
+      {sector.title} : {sector.sectors[0].Utilities}%
+        </p>
+      ))}
+  <br></br>
+     
+
     </div>
     
     
--- stock-sector-app/src/Sector.js
+++ stock-sector-app/src/Sector.js
@@ -1,15 +1,5 @@
 import React from 'react';
 
 
-function Sector({title}, {sectors}){
-    console.log(sectors)
-    return (
-    <li>
-        {title} - {sectors}
-    </li>
-
-    );
-}
-
 
 export default Sector;
